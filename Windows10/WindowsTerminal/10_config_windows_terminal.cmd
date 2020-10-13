868ddc771e216a6a1796027eb490885756ff2ff8
ooxxvv <ooxxvv@gmail.com>
2020-10-13 08:16:51 +0800

    Create lnk by powershell

--- Windows10/WindowsTerminal/10_config_windows_terminal.cmd
+++ Windows10/WindowsTerminal/10_config_windows_terminal.cmd
@@ -6,15 +6,24 @@ setlocal
 cd /d %~dp0
 set this_dir=%cd%
 set config_dir=%UserProfile%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
-set start_dir=%AppData%\Microsoft\Windows\Start Menu
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: Process :::
-echo "=============== Copy shortcuts to StartMenu ==========================="
-copy /y "%this_dir%\WSL.bat" "%start_dir%\WSL.bat"
-
 echo "=============== Copy settings.json ===================================="
 md "%config_dir%"
 copy /y "%this_dir%\settings.json" "%config_dir%\settings.json"
+
+echo "=============== Crate shortcuts to StartMenu ==========================="
+set lnk_file=%AppData%\Microsoft\Windows\Start Menu\windowsTerminal.lnk
+set exe_file=%LocalAppData%\Microsoft\WindowsApps\wt.exe
+set ico_file=%SystemDrive%\TotalCommander\ooxxvv\LISTooxxvv.icl
+
+set ps_cmd=$s=(New-Object -ComObject WScript.Shell).CreateShortcut('%lnk_file%');
+set ps_cmd=%ps_cmd% $s.TargetPath='%exe_file%';
+set ps_cmd=%ps_cmd% $s.Arguments='';
+set ps_cmd=%ps_cmd% $s.IconLocation='%ico_file%, 2';
+set ps_cmd=%ps_cmd% $s.Save()
+powershell "%ps_cmd%"
+echo "Created: %lnk_file%"
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 echo "========== All Done ==================================================="
